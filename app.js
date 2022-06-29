9c802bca99f636e6351e66a61402a42431748780
yaahsin <brillanteoboe@gmail.com>
2022-06-29 22:32:12 +0800

    connect todo and user

--- app.js
+++ app.js
@@ -6,13 +6,43 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// route setting
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
---
+++ models/todo.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Todo = sequelize.define('Todo', {
+    name: DataTypes.STRING,
+    isDone: DataTypes.BOOLEAN
+  }, {});
+  Todo.associate = function (models) {
+    Todo.belongsTo(models.User)
+  };
+  return Todo;
+};
