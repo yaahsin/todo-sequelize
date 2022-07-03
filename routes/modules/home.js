2fb927ec96fb6d4094febec301ceb51577ad4532
yaahsin <brillanteoboe@gmail.com>
2022-07-03 13:23:24 +0800

    feat: create function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,12 +5,20 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
+  const UserId = req.user.id
+  User.findByPk(UserId)
+    .then((user) => {
+      if (!user) throw new Error('user not found')
+
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId } //多重篩選條件
+      })
+    })
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,24 +6,50 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  const isDone = false
+
+  return Todo.create({ name, UserId, isDone })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
+
+  return Todo.findOne({
+    where: { id, UserId }
+  })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
 
 router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
+
+  return Todo.findOne({
+    where: { id, UserId }
+  })
     .then(todo => res.render('edit', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
 
 router.put('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
   const { name, isDone } = req.body
-  return Todo.findByPk(id)
+
+  return Todo.findOne({
+    where: { id, UserId }
+  })
     .then(todo => {
       todo.name = name
       todo.isDone = isDone === 'on'
